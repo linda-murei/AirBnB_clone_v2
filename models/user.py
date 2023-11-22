e4f018f3037f416c6d01e0653be8111bfc1cbbe9
linda-murei <lindajepleting@gmail.com>
2023-11-22 10:06:02 -0800

    task 8

--- models/user.py
+++ models/user.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
+from models.place import Place
+from models.review import Review
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    '''
+        Definition of the User class
+    '''
+    __tablename__ = 'users'
+    if storage_type == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        places = relationship("Place", backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete-orphan")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
